dead85ebf5ddaab250f443744de9f13c77d4b52a
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2026-04-05 04:07:13 +0900

    Increase orientation_potential (reward mode) and feet_parallel_to_ground weights

--- k1_walk/k1_walk/source/k1_walk/k1_walk/tasks/manager_based/k1_walk/k1_walk_env_cfg.py
+++ k1_walk/k1_walk/source/k1_walk/k1_walk/tasks/manager_based/k1_walk/k1_walk_env_cfg.py
@@ -333,11 +333,11 @@ class K1Rewards:
 
     orientation_potential = RewTerm(
         func=mdp.orientation_potential,
-        weight=-1e-2,
+        weight=1.0,
         params={
-            "sigma": 0.04, 
+            "sigma": 0.2,
             "discount_factor": 0.985,
-            "enable_potential": False, # falseにするとexpが無効になる
+            "enable_potential": False,
             }
     )
 
@@ -355,11 +355,11 @@ class K1Rewards:
     )
 
     feet_parallel_to_ground = RewTerm(
-        func=mdp.feet_parallel_to_ground, weight=3.0 * 0.1,
+        func=mdp.feet_parallel_to_ground, weight=3.0,
         params={
             "sigma": 0.1,
             "discount_factor": 0.985,
-            "enable_potential": True,
+            "enable_potential": False,
                 }
     )
 
@@ -463,7 +463,7 @@ class CurriculumCfg:
         func=mdp.modify_reward_weight_by_episode_length_linearly,
         params = {
             "term_name" : "orientation_potential",
-            "target_weight" : -5.0
+            "target_weight" : 2.0
         }
     )
 
