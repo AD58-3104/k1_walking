d79eb114e0ea335743041a158c91b62c15d6f316
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2026-04-04 14:08:23 +0900

    docs: コメントに統合

--- k1_walk/k1_walk/scripts/fast_sac/runner.py
+++ k1_walk/k1_walk/scripts/fast_sac/runner.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 """
-ランナースクリプト: 設定ファイルに従ってコマンドを順番に実行する
+次のpython形式で書かれた設定に従って、BASE_COMMAND + COMMON_ARGS + ADDITIONAL_ARGS + arg_list[i]のコマンドを順番に実行する。
+
+```python
+BASE_COMMAND = "bash play.sh"
+COMMON_ARGS = " --task k1-walk"
+ADDITIONAL_ARGS = ""   # コマンドラインからの追加引数を受け取る
+
+arg_list =[
+    [""],
+    [""],
+    [""],
+    [""],
+]
+```
+
+引数は以下のものを受け取る。
+- 読み込む対象の設定ファイルのファイル名
+- 追加共通コマンド
+
+引数がおかしいなどで、コマンドの実行が失敗した場合でもその場でスクリプトを停止させず、最後の実験まで実行する。
+ログを保存するディレクトリの名前は、「設定ファイルのファイル名_exp_実験番号」とする。
 """
 
 import argparse
