38e70a4f4e008f47a8f1a1afbf72bc35feaf5f65
AD58-3104 <s19c1012sk@s.chibakoudai.jp>
2026-04-05 03:26:15 +0900

    holosomaを真似て4096にした

--- k1_walk/k1_walk/scripts/fast_sac/train_for_llm.sh
+++ k1_walk/k1_walk/scripts/fast_sac/train_for_llm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ~/.bash_functions
 
-_labpython train.py --task K1-Walk-Train-fast-sac --max_iterations 2200 --num_envs 8196 --headless "$@" > run.log 2>&1
+_labpython train.py --task K1-Walk-Train-fast-sac --max_iterations 2200 --num_envs 4096 --headless "$@" > run.log 2>&1
 if [ $? -ne 0 ]; then
     echo "Running failed. You should check the run.log file for details."
 else
